b4e7991aacde08b0ec2532e6d2509eaff530dc5d
lvp <lvp@lvp-945GCM-S2C.(none)>
2013-07-23 01:50:44 -0500

    login no verify

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,16 @@ class UsersController < ApplicationController
     if login
       flash[:notice] = "login successfully"
       @user = User.find_by_name(params[:username])
-      redirect_to :action => 'index',@user => @user
+      redirect_to :action => 'index',:id => @user
     else
       flash[:notice] = "wrong username or password"
     end    
   end
   
   def index
-    
+    if params[:id].present?
+    @user = User.find_by_id(params[:id]) 
+    end
   end
 
   def mypage
